32c63c2c1e69c50a5758dd5286ee352cb9bb2f99
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-05-21 11:29:27 +0530

    formate

--- routes/Service/EventManager.js
+++ routes/Service/EventManager.js
@@ -1,35 +1,35 @@
 class EventManager {
-    constructor() {
-      this.events = []; 
-    }
-  
-    addEvent(date, startHour, endHour) {
-      const newEvent = { date, startHour, endHour };
-      for (const event of this.events) {
-        if (event.date === date && this.isOverlapping(event, newEvent)) {
-          return false; 
-        }
-      }
-      this.events.push(newEvent);
-      return true; 
-    }
-  
-    isOverlapping(event1, event2) {
-      return (
-        (event1.startHour < event2.endHour && event1.endHour > event2.startHour)
-      );
-    }
-    getEvents() {
-      return this.events.map(event => ({
-        date: event.date,
-        start_time: event.startHour,
-        end_time: event.endHour
-      }));
-    }
+  constructor() {
+    this.events = [];
+  }
 
-    deleteEvent(date, startHour, endHour) {
+  addEvent(date, startHour, endHour) {
+    const newEvent = { date, startHour, endHour };
+    for (const event of this.events) {
+      if (event.date === date && this.isOverlapping(event, newEvent)) {
+        return false;
+      }
+    }
+    this.events.push(newEvent);
+    return true;
+  }
+
+  isOverlapping(event1, event2) {
+    return (
+      event1.startHour < event2.endHour && event1.endHour > event2.startHour
+    );
+  }
+  getEvents() {
+    return this.events.map((event) => ({
+      date: event.date,
+      start_time: event.startHour,
+      end_time: event.endHour,
+    }));
+  }
+
+  deleteEvent(date, startHour, endHour) {
     const index = this.events.findIndex(
-      event =>
+      (event) =>
         event.date === date &&
         event.startHour === startHour &&
         event.endHour === endHour
@@ -41,7 +41,6 @@ class EventManager {
     }
     return false;
   }
-  }
-  
-  module.exports = EventManager;
-  
+}
+
+module.exports = EventManager;
